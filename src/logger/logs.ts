6b3bb9102ad058726a7eb0674e07059457e112ce
Anthony Yang <ay339@cornell.edu>
2021-02-08 01:03:22 -0500

    Workaround escape characters

--- src/logger/logs.ts
+++ src/logger/logs.ts
@@ -5,7 +5,7 @@ const credentials = {
     type: "service_account",
     project_id: "harmonylang-server",
     private_key_id: process.env.PRIVATE_KEY_ID,
-    private_key: process.env.PRIVATE_KEY,
+    private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
     client_email: process.env.CLIENT_EMAIL,
     client_id: process.env.CLIENT_ID,
     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
