a45a5a0a34f992a81dad94cd68424a0de0b93573
Anthony Yang <ay339@cornell.edu>
2021-03-24 00:22:02 -0400

    Add pid in start log

--- src/index.ts
+++ src/index.ts
@@ -123,6 +123,7 @@ async function buildApp() {
     const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
+        console.log(`Running as process ${process.pid}`);
     });
 }
 
