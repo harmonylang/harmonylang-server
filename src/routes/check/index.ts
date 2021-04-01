2f6eacee6c24862d817952cd9cffb9891b3fa8d3
Anthony Yang <ay339@cornell.edu>
2021-04-01 15:55:27 -0400

    Rate Limit 3 per minute

--- src/routes/check/index.ts
+++ src/routes/check/index.ts
@@ -91,8 +91,8 @@ export function makeCheckHandler(
             next();
         },
         rateLimit({
-            windowMs: 15 * 60 * 1000, // 15 minutes
-            max: 100 // limit each IP to 100 requests per windowMs
+            windowMs: 60 * 1000, // 15 minutes
+            max: 3 // limit each IP to 100 requests per windowMs
         }),
         upload.single("file"),
         async function(req: express.Request, res: express.Response) {
